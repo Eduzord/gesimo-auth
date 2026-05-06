92bfb0947efcff26fad6ed8a811431dcfeeb7666
Eduardo Oliveira <emoliveira97@gmail.com>
2026-05-06 20:38:42 -0300

    Informações de login ao banco no prisma.service são carregadas do .env agora

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,17 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
+  constructor(private configService: ConfigService) {
     const adapter = new PrismaMariaDb({
-      host: 'localhost',
-      port: 3306,
-      user: 'root', // Padrão do XAMPP
-      password: '', // Padrão do XAMPP
-      database: 'auth_db', // O banco que você criou no XAMPP para a API de auth
+      host: configService.get<string>('DB_HOST'), // Host do banco de dados
+      port: configService.get<number>('DB_PORT'), // Porta do banco de dados
+      user: configService.get<string>('DB_USER'), // Usuário do banco de dados
+      password: configService.get<string>('DB_PASSWORD'), // Senha do banco de dados
+      database: configService.get<string>('DB_NAME'), // O banco que você criou no XAMPP para a API de auth
     });
     
     super({ adapter });
